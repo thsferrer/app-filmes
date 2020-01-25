2c420fb9e60301caecd40416a04d4e59f2cb7333
thsferrer <thsferrer@gmail.com>
2020-01-25 09:35:38 -0300

    Alterações 25.01.2020

--- app/src/androidTest/java/br/aula/database/BancoDadosHelper.kt
+++ app/src/androidTest/java/br/aula/database/BancoDadosHelper.kt
@@ -2,11 +2,18 @@ package br.aula.database
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import br.aula.database.ConstantsDb.FILMES_DB_NAME
+import br.aula.database.ConstantsDb.FILMES_DB_TABLE
 import org.jetbrains.anko.db.*
 
 class BancoDadosHelper(context: Context) :
     ManagedSQLiteOpenHelper(ctx = context ,
-        name = "filmes.db",  version = 1) {
+        name = FILMES_DB_NAME,  version = 1) {
+    private val scriptSQLCreate = arrayOf(
+        "INSERT INTO $FILMES_DB_TABLE VALUES(1,'Minha mãe é uma peça',NULL, 'Filme de 2020');",
+        "INSERT INTO $FILMES_DB_TABLE VALUES(2,'The Edge of 17',NULL, 'Filme de 2020');",
+        "INSERT INTO $FILMES_DB_TABLE VALUES(3,'Fast and Furious',NULL, 'Filme de 2020');",
+        "INSERT INTO $FILMES_DB_TABLE VALUES(4,'TOC TOC',NULL, 'Filme de 2020');")
     //singleton da classe (obj é criado 1x e reutilizado)
     companion object {
         private var instance: BancoDadosHelper? = null
@@ -28,6 +35,10 @@ class BancoDadosHelper(context: Context) :
             "dtlancamento" to INTEGER,
             "descricao" to TEXT
         )
+         // insere dados iniciais na tabela
+       scriptSQLCreate.forEach {sql ->
+           db.execSQL(sql)
+       }
     }
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         TODO("not implemented")
---
+++ app/src/androidTest/java/br/aula/database/ConstantsDb.kt
@@ -0,0 +1,6 @@
+package br.aula.database
+
+object ConstantsDb {
+    const val FILMES_DB_NAME = "filmes.db"
+    const val FILMES_DB_TABLE = "filmes"
+}
