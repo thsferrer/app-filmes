418f2c72befae0863cbe800ca2e058d6d4f29d42
thsferrer <thsferrer@gmail.com>
2020-01-25 10:21:04 -0300

    Alterações 25.01.2020

--- app/src/androidTest/java/br/aula/database/Filme.kt
+++ app/src/androidTest/java/br/aula/database/Filme.kt
@@ -3,13 +3,9 @@ import java.io.Serializable
 
 data class Filme (
     var id: Long = 0,
-    var foto: String? = null,
     var nome: String? = null,
-    var endereco: String? = null,
-    var telefone: Long? = null,
-    var dataNascimento: Long? = null,
-    var email: String? = null,
-    var site: String? = null ) : Serializable {
+    var dtlancamento: Long? = null,
+    var descricao: String? = null ) : Serializable {
 
         override fun toString(): String {
             return nome.toString()
